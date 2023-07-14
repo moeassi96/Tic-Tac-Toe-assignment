2994f7fdd5642e256d032cc333318ef491a7c09c
moeassi96 <117896769+moeassi96@users.noreply.github.com>
2023-07-14 12:20:52 +0300

    finalized version

--- index.js
+++ index.js
@@ -3,14 +3,14 @@ let player2Score = 0;
 round = 0
 
 let win = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
 ];
 
 let startBtn = document.getElementById("start-btn");
@@ -23,19 +23,19 @@ let currentPlayer = "X";
 let board = ["", "", "", "", "", "", "", "", ""];
 
 
-function handleStart(){
+function handleStart() {
     let gameboard = document.getElementById("game-board");
     gameboard.addEventListener("click", handleClickStart)
 
     let playerXName = document.getElementById("PlayerX-name").value;
     let playerOName = document.getElementById("PlayerO-name").value;
 
-    document.getElementById("name-X").innerText = playerXName
-    document.getElementById("name-O").innerText = playerOName
+    document.getElementById("name-X").innerText = (playerXName != "") ? playerXName : "Player 1" 
+    document.getElementById("name-O").innerText = (playerOName != "") ? playerOName : "Player 2" 
     document.getElementById("scoreX").innerText = 0
     document.getElementById("scoreO").innerText = 0
 }
-function handleClickStart(e){
+function handleClickStart(e) {
     let clickedDiv = e.target;
     let divId = parseInt(clickedDiv.id);
 
@@ -45,80 +45,80 @@ function handleClickStart(e){
     }
     if (checkForWin(currentPlayer)) {
         round++;
-        setTimeout(function() {
-          updateScore(currentPlayer);
-          resetBoard();
+        setTimeout(function () {
+            updateScore(currentPlayer);
+            resetBoard();
         }, 3000);
         return;
-      }
-    if(checkForDraw()){
+    }
+    if (checkForDraw()) {
         resetBoard();
         return
     }
     currentPlayer = (currentPlayer === "X") ? "O" : "X"
-    }
-// check if won
-function checkForWin(player){
-    for(let i = 0; i<win.length; i++){
-        if(board[win[i][0]] == player && board[win[i][1]] == player && board[win[i][2]] == player){
+}
+
+function checkForWin(player) {
+    for (let i = 0; i < win.length; i++) {
+        if (board[win[i][0]] == player && board[win[i][1]] == player && board[win[i][2]] == player) {
             strikeThrough(i);
             return true;
         }
     }
     return false
 }
-// check if draw
-function checkForDraw(){
-    for(let i = 0; i<board.length; i++){
-        if(board[i] === ""){
+
+function checkForDraw() {
+    for (let i = 0; i < board.length; i++) {
+        if (board[i] === "") {
             return false
         }
     }
     return true
 }
-// update score
-function updateScore(player){
+
+function updateScore(player) {
     let score = document.getElementById("score" + player);
     score.innerText = parseInt(score.innerText) + 1;
-    nameOfWinner = document.getElementById("name-"+player).innerText
-    document.getElementById("update-text").innerText = nameOfWinner+ " won round " + round
+    nameOfWinner = document.getElementById("name-" + player).innerText
+    document.getElementById("update-text").innerText = nameOfWinner + " won round " + round
 }
-// resetBoard
-function resetBoard(){
+
+function resetBoard() {
     board = ["", "", "", "", "", "", "", "", ""];
     currentPlayer = "X"
-    for(let i = 0; i<board.length; i++){
+    for (let i = 0; i < board.length; i++) {
         document.getElementById(String(i)).innerText = ""
-    }   
+    }
 }
 
 function strikeThrough(cells) {
     if (JSON.stringify(win[cells]) === JSON.stringify([0, 4, 8])) {
-      for (let i = 0; i < 3; i++) {
-        document.getElementById(JSON.stringify(win[cells][i])).innerHTML =
-          currentPlayer + ' <div class="diagonal"></div>';
-      }
-    } else if (JSON.stringify(win[cells]) === JSON.stringify([2, 4, 6])){
         for (let i = 0; i < 3; i++) {
             document.getElementById(JSON.stringify(win[cells][i])).innerHTML =
-              currentPlayer + ' <div class="reverse-diagonal"></div>';
-          }
+                currentPlayer + ' <div class="diagonal"></div>';
+        }
+    } else if (JSON.stringify(win[cells]) === JSON.stringify([2, 4, 6])) {
+        for (let i = 0; i < 3; i++) {
+            document.getElementById(JSON.stringify(win[cells][i])).innerHTML =
+                currentPlayer + ' <div class="reverse-diagonal"></div>';
+        }
     }
-    else if ((JSON.stringify(win[cells]) === JSON.stringify([0, 1, 2])) || (JSON.stringify(win[cells]) === JSON.stringify([3, 4, 5])) || (JSON.stringify(win[cells]) === JSON.stringify([6, 7, 8]))  ){
+    else if ((JSON.stringify(win[cells]) === JSON.stringify([0, 1, 2])) || (JSON.stringify(win[cells]) === JSON.stringify([3, 4, 5])) || (JSON.stringify(win[cells]) === JSON.stringify([6, 7, 8]))) {
         for (let i = 0; i < 3; i++) {
             document.getElementById(JSON.stringify(win[cells][i])).innerHTML =
-              currentPlayer + ' <div class="horizontal"></div>';
-          }
-    }else{
+                currentPlayer + ' <div class="horizontal"></div>';
+        }
+    } else {
         for (let i = 0; i < 3; i++) {
             document.getElementById(JSON.stringify(win[cells][i])).innerHTML =
-              currentPlayer + ' <div class="vertical"></div>';
-          }
+                currentPlayer + ' <div class="vertical"></div>';
+        }
     }
 
 
 }
-function handleReset(){
+function handleReset() {
     player1Score = 0;
     player2Score = 0;
     document.getElementById("scoreX").innerText = 0
